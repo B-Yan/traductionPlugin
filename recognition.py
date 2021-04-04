2f47d4285d9041559e7f4a55aa497882c4801979
antoine <31598298+TonyBoy22@users.noreply.github.com>
2021-04-03 23:27:09 -0400

    Timestamps works in recognition

--- recognition.py
+++ recognition.py
@@ -1,25 +1,58 @@
 import speech_recognition as sr
 import pocketsphinx
+import pydub
 
 # L'idée est de faire une boucle for pour analyser
 # le contenu audio brin par brin avec un certain
 # chevauchement entre les segments pour éviter les
 # coupures
+OFFSET          = 0
+CHUNK_LEN       = 10
+OVERLAP         = 2
+STEP            = CHUNK_LEN-OVERLAP
+text_register   = [] # On veut une liste avec un objet text et un index du segment analysé
+completed       = False
+counter         = 0
 
-text_register = [] # On veut une liste avec un objet text et un index du segment analysé
+def get_timestamp(text_register: list, input: str):
+    # Check if the input substring can be found in the text_register strings
+    timestamps_list = []
+    # Go through each line of the text_register
+    for i in range(len(text_register)):
+        if text_register[i][1].find(input) != -1:
+            # Add the index in text_register converted in seconds
+            timestamps_list.append(i*STEP)
+    return timestamps_list
 
-with sr.AudioFile('sample_phone_call_youtube.wav') as source:
+# get user input
+user_input = input('What word are you looking for?')
 
-    try:
+while not completed:
+    # Will mark completed when total offset + duration will be > file_duration
+
+    with sr.AudioFile('sample_phone_call_youtube.wav') as source:
         r = sr.Recognizer()
-        r.energy_threshold = 300
-
+        file_length = source.DURATION  # float
+        #r.energy_threshold = 300
+        r.adjust_for_ambient_noise(source)
         # Meme si on ouvre le fichier audio, il faut quand même l'enregistrer dans la classe recognizer
-        audio = r.record(source)
+        audio = r.record(source, offset=OFFSET, duration=CHUNK_LEN)
 
-        source.DURATION # float de la durée
-        text = r.recognize_sphinx(audio_data=audio, language='en-US')
-        print(text)
+        text = r.recognize_sphinx(audio_data=audio, language='en-US') #, show_all=True) # Show All montre toutes \
+        # les traductions probables du fichier
+        print('text: ', text)
+        OFFSET += STEP
 
-    finally:
-        print('Program stopping')
+        if OFFSET + CHUNK_LEN > file_length:
+            completed = True
+            break
+    counter += 1
+    text_register.append([counter, text])
+
+timestamps_list = get_timestamp(text_register, user_input)
+if not timestamps_list:
+    print('no matches!')
+else:
+    print('matches at: ', timestamps_list)
+
+
