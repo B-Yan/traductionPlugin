41cb433b6ae8810d9bd39d5f8bf813225084b504
Yannick Bellerose <yan-bel@hotmail.com>
2022-03-18 16:26:24 -0400

    a working version of frontend

--- src/frontend/readContent.js
+++ src/frontend/readContent.js
@@ -5,10 +5,9 @@
  * @author Yannick Bellerose
  * @version 1.0.0
  */
-var url = chrome.tabs.query({active:true},function(tab){return tab[0].url;});
-var myJson;
 var theForm;
 var theText;
+var theVal;
 
 window.onload = function(){
     theForm = document.getElementById("theForm");
@@ -21,23 +20,25 @@ window.onload = function(){
     theForm.onsubmit = searchIt;
 }
 
-
-
 /**
  * Gets called when chrome.storage change. 
  * If it's a json it means we have received a valid response from the server. We then call createHtml.
  * Otherwise we simplys display the content of chrome.storage
  */
 function getUrl(){
-    chrome.storage.local.get(url, function(result){
-        str = result.url;
-        try {
-            myJson = JSON.parse(str);
-            theForm.style.display="block";
-            theText.display = "none";
-        } catch (e) {
-            theText.textContent = str;
-        }   
+    chrome.tabs.query({active:true, currentWindow: true},function(tab){
+        console.log("INFO: getUrl - "+tab[0].url);
+        var pageUrl = tab[0].url;
+        chrome.storage.local.get(pageUrl, function(result){  
+            console.log("INFO: getUrl - " + result[pageUrl])       
+            var str = result[pageUrl];
+            if (typeof str === 'string' || str instanceof String) {
+                searchDisplay(str);
+            } else {
+                displayAnswerLoaded();
+            }   
+        
+        });
     });
 }
 
@@ -50,16 +51,48 @@ function getUrl(){
  */
 function searchIt(event){
     event.preventDefault();
-    theText.display = "block";
+    chrome.tabs.query({active:true, currentWindow: true},function(tab){
+        var pageUrl = tab[0].url;
+        chrome.storage.local.get(pageUrl, function(result){         
+            var myJson = result[pageUrl];
+            if (typeof myJson === 'string' || myJson instanceof String) {
+                console.log("ERROR: The transcription had a problem in the frontend");
+                searchDisplay("ERROR: The transcription had a problem in the frontend");
+            } else {
+                displayResults(analyzeJson(myJson, theVal.value), theVal.value); 
+            }   
+        
+        });
+    });
+}
+
+function analyzeJson(json, word){
     var results = [];
-    for (var i = 0; i<myJson.list.length; i++){
-        if (myJson.list[i].includes(theVal.value)){
-            results.push(i*myJson.step);
+    for (var i = 0; i<json.list.length; i++){
+        if (json.list[i].includes(word)){
+            results.push(i*json.step);
+            console.log("INFO: analyzeJson - " + i*json.step);
         }
     }
+    return results;
+}
+
+function displayResults(results, word){
     if (results.length == 0){
-        theText.textContent = "This word " + theVal.value + " doesn't appear in the video";
+        searchDisplay("This word " + word + " doesn't appear in the video");
     } else {
-        theText.textContent = "The word " + theVal.value + " appears at time: " + results.toString() + " seconds";
+        searchDisplay("The word " + word + " appears at time: " + results.join(" - ") + " seconds");
     }
+}
+
+function searchDisplay(text){
+    theText.style.display = "block";
+    theText.textContent = text;
+    console.log("INFO: searchDisplay - "+text);
+}
+
+function displayAnswerLoaded(){
+    theForm.style.display="block";
+    theText.style.display = "none";
+    console.log("INFO: displayAnswerLoaded");
 }
